94335a2f78d84c7ea335de047d833e8f0fc9cb73
surabhikislaya <surabhi.123.sk@gmail.com>
2013-09-05 21:47:03 +0530

    Create hello.m

---
+++ hello.m
@@ -0,0 +1,17 @@
+#import "hello.h"
+
+@implementation hello
+
+- addStringValue:(const char*)astring
+	{
+		strcat(buffer, astring);
+		return 0;
+	}
+	
+- print
+   {
+		printf("%s\n", buffer);
+		return 0;
+	}
+	
+@end
